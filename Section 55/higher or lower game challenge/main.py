4b80f42f548c6058c8e43804a24721051cd1d6ab
vxntcy <joyyg0606@gmail.com>
2024-08-16 18:10:40 -0400

    improved version of my higher or lower game end

--- Section 55/higher or lower game challenge/main.py
+++ Section 55/higher or lower game challenge/main.py
@@ -1,30 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template_string
 import random
-
-random_number = random.randint(0, 9)
-print(random_number)
 
 app = Flask(__name__)
 
+random_number = random.randint(0, 9)
+print(f"Random Number: {random_number}")
+
+# HTML Templates
+home_page = """
+<h1>Guess a number between 0 and 9</h1>
+<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif"/>
+"""
+
+high_page = """
+<h1 style="color: purple">Too high, try again!</h1>
+<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif"/>
+"""
+
+low_page = """
+<h1 style="color: red">Too low, try again!</h1>
+<img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif"/>
+"""
+
+found_page = """
+<h1 style="color: green">You found me!</h1>
+<img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif"/>
+"""
 
 @app.route('/')
 def home():
-    return "<h1>Guess a number between 0 and 9</h1>" \
-           "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
+    return render_template_string(home_page)
 
 
-@app.route("/<int:guess>")
+@app.route("/<guess>")
 def guess_number(guess):
-    if guess > random_number:
-        return "<h1 style='color: purple'>Too high, try again!</h1>" \
-               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+    try:
+        guess = int(guess)
+    except ValueError:
+        return "<h1 style='color: orange'>Invalid input! Please enter a number.</h1>"
 
+    if guess > random_number:
+        return render_template_string(high_page)
     elif guess < random_number:
-        return "<h1 style='color: red'>Too low, try again!</h1>"\
-               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+        return render_template_string(low_page)
     else:
-        return "<h1 style='color: green'>You found me!</h1>" \
-               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
+        global random_number
+        random_number = random.randint(0, 9)  # Reset the number after a win
+        print(f"New Random Number: {random_number}")
+        return render_template_string(found_page)
 
 
 if __name__ == "__main__":
