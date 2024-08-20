891efe612b229899f406c73a9de6d16d43d585ef
vxntcy <joyyg0606@gmail.com>
2024-08-20 14:48:51 -0400

    adding styling challenge half

--- Section 59/Adding Styling Challenge/main.py
+++ Section 59/Adding Styling Challenge/main.py
@@ -1,21 +1,11 @@
-from flask import Flask, render_template
+## MORE CODE ABOVE
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def get_all_posts():
-    return render_template("index.html")
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+  
+  ## MORE CODE BELOW
